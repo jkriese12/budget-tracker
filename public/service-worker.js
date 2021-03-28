3fa5c5252a5af0a8f29504737bf6e7631daf563b
Jacob Kriese <jkriese12@gmail.com>
2021-03-28 08:27:51 -0700

    Service worker static cache added

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,22 @@
-console.log("Hey this is working");
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/index.html",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/styles.css",
+];
+
+// install
+self.addEventListener("install", function (evt) {
+  // pre cache image data
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+  // pre cache all static assets
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});
